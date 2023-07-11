7ac3eff1124e6057718ca82d24fe18572d58e112
Venkata Rami R.B <vreddy.ui@gmail.com>
2023-07-11 20:45:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
 provider "aws" {
   region     = "ap-south-1"
 }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
 
 resource "aws_instance" "Demo" {
   ami           = "ami-053b0d53c279acc90"
