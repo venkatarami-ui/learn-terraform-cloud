3a53d7ab4f1378ca74df1b6c6e11b473fe042b96
Venkata Rami Reddy <vreddy.ui@gmail.com>
2023-07-17 17:33:46 +0530

    added keypar AWSKeyForTerrformCloud

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+
 provider "aws" {
-  region = "ap-south-1"
+  region     = "ap-south-1"
+  access_key = "AKIA26BZ37GBMM2SREUK"
+  secret_key = "bvB1SW+h2VIFrFCg1kE3CuZkkOIdDgf/5gaH+94W"
 }
+
 
 resource "aws_instance" "example_instance" {
   ami           = "ami-0f5ee92e2d63afc18"
