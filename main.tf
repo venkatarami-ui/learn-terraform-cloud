bffb8bddf5baeef3d3575e804099011d62b2d0b2
Venkata Rami Reddy <vreddy.ui@gmail.com>
2023-07-17 16:40:28 +0530

    added keypar AWSKeyForTerrformCloud

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "example_instance" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
-  key_name      = "DevOpsHint"
+  key_name      = "AWSKeyForTerrformCloud"
   tags = {
     Name = "Demos"
   }
